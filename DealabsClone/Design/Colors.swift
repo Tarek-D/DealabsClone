2fa0e1519adfc22b49595d3de91e3bb9e718cdad
Tarek Noubli <tareknoubli@gmail.com>
2024-07-31 21:04:22 +0200

    add: - logo asset - Color extension - First steps of the homeView

---
+++ DealabsClone/Design/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  DealabsClone
+//
+//  Created by Tarek Noubli on 31/07/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primaryBlue = Color(red: 0.011764705882352941, green: 0.6470588235294118, blue: 0.7568627450980392)
+}
+ // rgba(3,165,193,255)
--- DealabsClone/Views/HomeView.swift
+++ DealabsClone/Views/HomeView.swift
@@ -8,16 +8,86 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var selectedTab = HomeTabMenuOptions.all[0]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HeaderHomeView()
+                .padding(.bottom)
+            HomeTabMenuView(selectedTab: $selectedTab)
         }
         .padding()
     }
 }
+
+struct HeaderHomeView: View {
+    var body: some View {
+        HStack {
+            Button(action: {}, label: {
+                Image(systemName: "line.3.horizontal")
+            })
+            Spacer()
+            Image("Dealabs_Logo.svg")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100)
+                .padding(.trailing, 80)
+            Button(action: {}, label: {
+                Image(systemName: "plus.square")
+            })
+            Button(action: {}, label: {
+                Image(systemName: "slider.horizontal.3")
+            })
+        }
+        .foregroundStyle(Color.primaryBlue)
+    }
+}
+
+struct HomeTabMenuView: View {
+    @Binding var selectedTab: String
+    @Namespace var namespace
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 20) {
+                ForEach(HomeTabMenuOptions.all, id: \.self) { option in
+                    Button(action: {
+                        withAnimation {
+                            selectedTab = option
+                        }
+                    },
+                           label: {
+                        VStack {
+                            Text(option)
+                                .font(.subheadline)
+                                .foregroundStyle(.gray)
+                            if option == selectedTab {
+                                Color.primaryBlue
+                                    .frame(height: 1)
+                                    .matchedGeometryEffect(id: "underline", in: namespace, properties: .frame)
+                            } else {
+                                Color.clear.frame(height: 1)
+                            }
+                        }
+                    })
+                }
+            }
+        }
+        .scrollIndicators(.hidden)
+    }
+}
+
+struct HomeTabMenuOptions {
+    static let all = [
+        "A la une",
+        "Tendance",
+        "Tous",
+        "Les + hot",
+        "Commentés"
+    ]
+}
+
+
+
 
 #Preview {
     HomeView()
