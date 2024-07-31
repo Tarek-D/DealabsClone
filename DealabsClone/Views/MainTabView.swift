8d35b2daf2d3109946f794de65003ad7a0376bf0
Tarek Noubli <tareknoubli@gmail.com>
2024-07-31 16:21:15 +0200

    MainTabView preview

--- DealabsClone/Views/MainTabView.swift
+++ DealabsClone/Views/MainTabView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @State var selectedTab: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selectedTab) {
+            HomeView()
+                .tabItem {
+                    Label("", systemImage: "house")
+                }
+                .tag("Home")
+            NotificationBellView()
+                .tabItem {
+                    Label("", systemImage: "bell")
+                }
+                .tag("Notification")
+            SearchView()
+                .tabItem {
+                    Label("", systemImage: "magnifyingglass")
+                }
+                .tag("Search")
+            MessagingView()
+                .tabItem {
+                    Label("", systemImage: "envelope")
+                }
+                .tag("Messaging")
+            ProfilView()
+                .tabItem {
+                    Label("", systemImage: "person.crop.circle")
+                }
+                .tag("Profil")
+        }
     }
 }
 
 #Preview {
-    MainTabView()
+    MainTabView(selectedTab: "Home")
 }
