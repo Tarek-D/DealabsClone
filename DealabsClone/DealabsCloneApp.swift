e97fb64e69ee8daa994d17465cdc16d0b975b6e1
Tarek Noubli <tareknoubli@gmail.com>
2024-07-31 16:02:04 +0200

    Initial Commit

---
+++ DealabsClone/DealabsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DealabsCloneApp.swift
+//  DealabsClone
+//
+//  Created by Tarek Noubli on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct DealabsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
