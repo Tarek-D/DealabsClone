04084f309f1885f67579e4d14dcee20744f4266f
Tarek Noubli <tareknoubli@gmail.com>
2024-07-31 16:20:35 +0200

    set up main tab view

---
+++ DealabsClone/Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  DealabsClone
+//
+//  Created by Tarek Noubli on 31/07/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
